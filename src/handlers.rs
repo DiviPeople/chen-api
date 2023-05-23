102411bf380a6132fa8ef38b812821a8e7b102e6
V1ammer <v1lammer@gmail.com>
2023-05-23 14:13:36 +0300

    Add GET method for entries

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,7 @@
 use actix_web::{get, post, web, Responder, HttpResponse};
-use crate::models::Status;
+use sea_orm::EntityTrait;
+use crate::entity::prelude::Users;
+use crate::models::{AppState, Status};
 
 #[get("/")]
 pub async fn status() -> impl Responder {
@@ -7,12 +9,22 @@ pub async fn status() -> impl Responder {
         .json(Status {status: "UP".to_string()})
 }
 
-#[post("/create-new-user")]
+#[get("/entries")]
+async fn get_entries(data: web::Data<AppState>) -> impl Responder {
+    let conn = &data.conn;
+
+    let users: Vec<serde_json::Value> = Users::find().into_json().all(conn).await.unwrap();
+
+    HttpResponse::Ok().json(users)
+}
+
+#[post("/create-new-User")]
 pub async fn create_new_user() -> impl Responder {
     format!("hello from create_new_user")
 }
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(status)
-        .service(create_new_user);
+        .service(create_new_user)
+        .service(get_entries);
 }
