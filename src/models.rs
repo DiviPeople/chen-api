8ed37a541fc1271ee0552eb8a118a11442d01bd8
V1ammer <v1lammer@gmail.com>
2023-05-25 18:20:33 +0300

    Move status struct from models to serializers

--- src/models.rs
+++ src/models.rs
@@ -1,12 +1,6 @@
-use serde::Serialize;
 use sea_orm::DatabaseConnection;
 
 #[derive(Clone)]
 pub struct AppState {
     pub conn: DatabaseConnection,
-}
-
-#[derive(Serialize)]
-pub struct Status {
-    pub status: String,
 }
