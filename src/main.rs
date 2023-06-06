4a11e0ab71ab5cec1493b709ba31afed76e79a70
V1ammer <v1lammer@gmail.com>
2023-06-06 23:54:47 +0300

    Add permission check

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,17 @@ mod handlers;
 mod jwt_auth;
 mod serializers;
 
-use crate::config::{AppConfig, AppState, CorsConfig};
-use actix_web::{middleware::Logger, web::Data, App, HttpServer};
+use crate::{
+    config::{AppConfig, AppState, CorsConfig},
+    serializers::TokenClaims,
+};
+use actix_web::{
+    dev::ServiceRequest, error::ErrorUnauthorized, http, middleware::Logger, web::Data, App, Error,
+    HttpServer,
+};
+use actix_web_grants::GrantsMiddleware;
 use env_logger::Env;
+use jsonwebtoken::{decode, DecodingKey, Validation};
 use sea_orm::Database;
 use std::io;
 
@@ -29,8 +37,11 @@ async fn main() -> io::Result<()> {
     HttpServer::new(move || {
         let cors =
             CorsConfig::set_cors(&cors_config.protocol, &cors_config.host, &cors_config.port);
+
+        let auth = GrantsMiddleware::with_extractor(extract);
         App::new()
             .app_data(Data::new(AppState { conn: conn.clone() }))
+            .wrap(auth)
             .configure(handlers::config)
             .wrap(Logger::default())
             .wrap(cors)
@@ -38,4 +49,46 @@ async fn main() -> io::Result<()> {
     .bind(server_addr)?
     .run()
     .await
+}
+
+async fn extract(req: &ServiceRequest) -> Result<Vec<String>, Error> {
+    let config = AppConfig::from_env();
+
+    let token = req
+        .cookie("token")
+        .map(|c| c.value().to_string())
+        .or_else(|| {
+            req.headers()
+                .get(http::header::AUTHORIZATION)
+                .map(|h| h.to_str().unwrap().split_at(7).1.to_string())
+        });
+
+    let token = match token {
+        Some(token) => token,
+        None => {
+            return Ok(vec!["ROLE_USER".to_string()]);
+        }
+    };
+
+    let claims = match decode::<TokenClaims>(
+        &token,
+        &DecodingKey::from_secret(config.jwt_secret.as_ref()),
+        &Validation::default(),
+    ) {
+        Ok(c) => c.claims,
+        Err(_) => {
+            return Err(ErrorUnauthorized("You are not logged in"));
+        }
+    };
+
+    let is_superuser = claims.is_superuser;
+    let is_staff = claims.is_staff;
+
+    if is_superuser {
+        Ok(vec!["ROLE_SUPERUSER".to_string()])
+    } else if is_staff {
+        Ok(vec!["ROLE_STAFF".to_string()])
+    } else {
+        Ok(vec!["ROLE_INTERN".to_string()])
+    }
 }
